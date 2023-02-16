2d19b73dbcf0b9469eb3dfa4ef4d82d26d0d92d1
david alvarez <david@southfloridaathleticclub.com>
2023-02-16 00:35:48 -0500

    charfield

--- django/website/blog/models.py
+++ django/website/blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.TextField()
+    name = models.CharField()
     slug = models.SlugField(db_index=True)
 
     def __str__(self):
         return self.name
 
 class SubCategory(models.Model):
-    name = models.TextField()
+    name = models.CharField()
     slug = models.SlugField(db_index=True)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -23,18 +23,18 @@ class ReviewPost(models.Model):
     headline = models.TextField()
     intro = models.TextField()
     description = models.TextField()
-    product_label = models.TextField()
-    product_name = models.TextField()
+    product_label = models.CharField()
+    product_name = models.CharField()
     product_description = models.TextField()
-    product_affiliate_url = models.TextField()
+    product_affiliate_url = models.CharField()
     faq_answer_1 = models.TextField()
     faq_answer_2 = models.TextField()
     faq_answer_3 = models.TextField()
     faq_question_1 = models.TextField()
     faq_question_2 = models.TextField()
     faq_question_3 = models.TextField()
-    product_image_url = models.TextField()
-    product_image_alt = models.TextField()
+    product_image_url = models.CharField()
+    product_image_alt = models.CharField()
 
     def __str__(self):
         return self.title
