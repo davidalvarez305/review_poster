949e0d64d40818caf3ac560661afaef83f64f9ea
unknown <david@southfloridaathleticclub.com>
2023-02-17 03:36:53 -0500

    urls

--- django/website/blog/urls.py
+++ django/website/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:sub_category>/<slug:slug>/', views.review_post, name='review_post')
+    path('<slug:sub_category_slug>/<slug:slug>/', views.ReviewPostView.as_view(), name='review_post'),
+    path('<slug:category_slug>/<slug:sub_category_slug>/', views.SubCategoryView.as_view(), name='sub_category'),
+    path('<slug:category_slug>/', views.CategoryView.as_view(), name='category')
 ]
