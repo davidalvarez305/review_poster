35953ec522e5ac538d1527842634f69b43fd72b6
david alvarez <david@southfloridaathleticclub.com>
2023-04-05 02:05:07 -0400

    implemented auth middleware for protection

---
+++ crawler/server/middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthMiddleware(c *fiber.Ctx) error {
+	headers := c.GetReqHeaders()
+	auth := headers["Authorization"]
+
+	if auth != "Bearer "+os.Getenv("HEADER_STRING") {
+		return c.Status(401).JSON(fiber.Map{
+			"data": "Unauthorized request.",
+		})
+	}
+
+	return c.Next()
+}
--- crawler/server/routes/router.go
+++ crawler/server/routes/router.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/davidalvarez305/review_poster/crawler/server/controllers"
+	"github.com/davidalvarez305/review_poster/crawler/server/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Router(app *fiber.App) {
-	api := app.Group("api")
+	api := app.Group("api", middleware.AuthMiddleware)
 	controllers.Google(api)
 	controllers.Amazon(api)
 	controllers.ReviewPost(api)
