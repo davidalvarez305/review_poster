9e0d7183c6e5897217ee17431d01e8070f6544cd
david alvarez <david@southfloridaathleticclub.com>
2023-04-18 21:22:11 -0400

    header fixed

--- crawler/server/actions/content.go
+++ crawler/server/actions/content.go
@@ -23,6 +23,7 @@ func PullDynamicContent() (types.ContentAPIResponse, error) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+os.Getenv("AUTH_HEADER_STRING"))
+	req.Header.Set("X-SECRET-AGENT", os.Getenv("X_SECRET_AGENT"))
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -53,6 +54,7 @@ func PullContentDictionary() (types.DictionaryAPIResponse, error) {
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Bearer "+os.Getenv("AUTH_HEADER_STRING"))
+	req.Header.Set("X-SECRET-AGENT", os.Getenv("X_SECRET_AGENT"))
 
 	resp, err := client.Do(req)
 	if err != nil {
