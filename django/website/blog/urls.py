1c8aa0bad3ef8a5ac222f7616a16218c86bee863
unknown <david@southfloridaathleticclub.com>
2023-07-31 20:06:26 -0400

    REDIRECTS FOR OLD URLS

--- django/website/blog/urls.py
+++ django/website/blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('single/<str:slug>', RedirectView.as_view(url='/'), name='redirect_single'),
+    path('multiple/<str:slug>', RedirectView.as_view(url='/'), name='redirect_multiple'),
+    path('training/<str:slug>', RedirectView.as_view(url='/'), name='redirect_training'),
     path('<slug:category>/<slug:sub_category>/<slug:slug>', views.ReviewPostView.as_view(), name='review_post'),
     path('<slug:category>/<slug:sub_category>/', views.SubCategoryView.as_view(), name='sub_category'),
     path('<slug:category>/', views.CategoryView.as_view(), name='category'),
